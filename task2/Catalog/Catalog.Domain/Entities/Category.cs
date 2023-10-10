9a470c88047e2c9fbc4489f98f36cdb3d08d2696
Bertalan Kiss <bertalan.kiss@betssongroup.com>
2023-10-10 17:52:28 +0200

    Initial commit

---
+++ task2/Catalog/Catalog.Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Domain.Entities
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public Category Parent { get; set; }
+    }
+}
---
+++ task2/Catalog/Catalog.Domain/Entities/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain.Entities
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public Category Category { get; set; }
+        public decimal Money { get; set; }
+        public uint Amount { get; set; }
+    }
+}
